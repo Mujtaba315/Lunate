30a6618d530670fbf5c22fc080de8652c360a2a9
Syed Muhammad Mujtaba Naqvi <123009259+Mujtaba315@users.noreply.github.com>
2024-09-12 01:20:32 -0700

    Add files via upload

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,6 +1,19 @@
 import '../styles/components/signup.scss';
+import $ from 'jquery';
 
 function SignUp () {
+
+    function handleSubmitBtnMouseEnter (e) {
+        $(e.currentTarget).find('.submit-arrow-js').removeClass('on-mouse-out');
+        $(e.currentTarget).find('.submit-arrow-js').addClass('on-mouse-in');
+        
+    }
+
+    function handleSubmitBtnMouseLeave (e) {
+        $(e.currentTarget).find('.submit-arrow-js').removeClass('on-mouse-in');
+        $(e.currentTarget).find('.submit-arrow-js').addClass('on-mouse-out');
+    }
+
     return (
         <div className='signup-sec-wrapper'>
             <div className="image-wrapper">
@@ -69,6 +82,21 @@ function SignUp () {
                         <input type="checkbox" className='checkbox-input'/>
                         <span className="custom-checkbox"></span>
                         <label className="checkbox-label">To All Email Communications</label>
+                    </div>
+                </div>
+                <div className="cta-wrapper">
+                    <div className="description-wrapper">
+                        <p className="description">*Subscribing to any of the above will automatically subscribe you to occasional seasonal greeting emails (Eid, New Year, etc).</p>
+                    </div>
+                    <div className="submit-btn-wrapper">
+                        <button className='submit-btn' type="submit" onMouseEnter={handleSubmitBtnMouseEnter} onMouseLeave={handleSubmitBtnMouseLeave}>
+                            <span className="submit-text">Subscribe</span>
+                            <span className='submit-arrow-span'>
+                                <svg className='submit-arrow submit-arrow-js' width="0.938rem" height="0.938rem" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M8.35355 0.646447C8.15829 0.451184 7.84171 0.451184 7.64645 0.646447C7.45118 0.841709 7.45118 1.15829 7.64645 1.35355L13.7929 7.5H1.00195C0.725811 7.5 0.501953 7.72386 0.501953 8C0.501953 8.27614 0.725811 8.5 1.00195 8.5H13.7929L7.64645 14.6464C7.45118 14.8417 7.45118 15.1583 7.64645 15.3536C7.84171 15.5488 8.15829 15.5488 8.35355 15.3536L15.3282 8.37892C15.4346 8.28723 15.502 8.15148 15.502 8C15.502 7.84852 15.4346 7.71278 15.3282 7.62109L8.35355 0.646447Z"/>
+                                </svg>
+                            </span>
+                        </button>
                     </div>
                 </div>
             </form>
