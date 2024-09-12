3c2578e1584fd4fbb90c42138557edac55feaa46
Syed Muhammad Mujtaba Naqvi <123009259+Mujtaba315@users.noreply.github.com>
2024-09-12 04:20:01 -0700

    Add files via upload

--- src/components/LastSec.jsx
+++ src/components/LastSec.jsx
@@ -1,10 +1,12 @@
 import '../styles/components/lastsec.scss';
 import SignUp  from './SignUp';
+import Footer from './Footer';
 
 function LastSec () {
     return (
         <div className='last-sections-wrapper'>
             <SignUp />
+            <Footer />
         </div>
     );
 }
